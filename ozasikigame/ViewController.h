28fce16ea234e99ed75076181c6ee281adab3e72
Nanami Tomozoe <nanami@tomozoe.com>
2014-11-29 14:17:54 +0900

    Initial Commit

---
+++ ozasikigame/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ozasikigame
+//
+//  Created by nanami on 2014/11/29.
+//  Copyright (c) 2014年 nanami. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
